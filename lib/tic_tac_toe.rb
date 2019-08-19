d86db0e8cf595b7d80098d4446ce5423ee790928
Sharnell Grant <sharnellgrant@gmail.com>
2019-08-19 22:27:59 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,15 +1,34 @@
 # Defined the constant
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] 
 def display_board(board)
-puts " #{board[0]}| #{board[1]} | #{board[2]}"
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
-puts " #{board[3]}| #{board[4]} | #{board[5]}"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
 puts "-----------"
-puts " #{board[6]}| #{board[7]} | #{board[8]}"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
 def input_to_index(user_input)
-  user_input.to_i -1
+  user_input.to_i - 1
 end
+
+def move(board, index, player_token)
+  board[index] = player_token
+end
+
+def position_taken?(board, index)
+  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+    return false
+  else
+    return true
+  end
+end
+
+def valid_move?(board, index)
+ if index.between?(0,8) && !position_taken?(board, index)
+      return true
+  end
+end
+
+
